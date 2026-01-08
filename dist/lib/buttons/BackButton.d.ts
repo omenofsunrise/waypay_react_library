6aef70d37b4f474724d5d09e948c8888b03c44e1
KarpovetsIlya <164213460+KarpovetsIlya@users.noreply.github.com>
2026-01-08 15:59:27 +0300

    fix

---
+++ dist/lib/buttons/BackButton.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+interface BackButtonProps {
+    children: React.ReactNode;
+    onClick?: () => void;
+}
+declare const BackButtonComponent: React.FC<BackButtonProps>;
+export default BackButtonComponent;
