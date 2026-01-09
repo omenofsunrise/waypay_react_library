c0f455e9b8eabe8aea8f08a4a4d70b9afc56bbe0
KarpovetsIlya <164213460+KarpovetsIlya@users.noreply.github.com>
2026-01-10 00:41:04 +0300

    chore: stop tracking dist

--- dist/lib/inputs/ToggleSwitch.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-interface ToggleSwitchProps {
-    checked: boolean;
-    onChange: (checked: boolean) => void;
-    disabled?: boolean;
-}
-declare const ToggleSwitch: React.FC<ToggleSwitchProps>;
-export default ToggleSwitch;
--- dist/lib/modal/AuthCallModal.d.ts
+++
@@ -1,8 +0,0 @@
-type Props = {
-    isOpen: boolean;
-    callPhone: string;
-    error?: string;
-    onClose?: () => void;
-};
-declare const AuthCallModal: ({ isOpen, callPhone, error, onClose }: Props) => import("react/jsx-runtime").JSX.Element | null;
-export default AuthCallModal;
