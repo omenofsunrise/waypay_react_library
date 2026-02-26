229ee4cc6acef6e279ea2b629e98ffa191441717
omenofsunrise <ch3rnow.c@yandex.ru>
2026-02-26 23:37:24 +0300

    add modal containers

---
+++ dist/lib/containers/InputComponents.d.ts
@@ -0,0 +1,3 @@
+export declare const InputRow: import("styled-components/dist/types").IStyledComponentBase<"web", import("styled-components").FastOmit<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, never>> & string;
+export declare const InputLabel: import("styled-components/dist/types").IStyledComponentBase<"web", import("styled-components").FastOmit<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>, never>> & string;
+export declare const InputContainer: import("styled-components/dist/types").IStyledComponentBase<"web", import("styled-components").FastOmit<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, never>> & string;
---
+++ src/lib/containers/InputComponents.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const InputRow = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  margin-bottom: 16px;
+`;
+
+export const InputLabel = styled.span`
+  text-align: left;
+  font-size: 14px;
+  color: black;
+  max-width: 90px;
+`;
+
+export const InputContainer = styled.div`
+  margin-left: auto;
+  flex: 1;
+  max-width: 425px;
+  margin-top: 10px;
+`;
